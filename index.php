484bf4850eebf5076e69a84ae8627fffb65b7a54
Alan Silva <alansilvab@yahoo.com.mx>
2026-05-13 20:51:12 -0400

    changes on file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portafolio</title>
+</head>
+<body>
+
+<header>
+    <h1>Built With Purpose, Designed With Passion</h1>
+    <p class="tagline">A showcase of work that speaks louder than words</p>
+</header>
+
+
+
+  
+</body>
+</html>
